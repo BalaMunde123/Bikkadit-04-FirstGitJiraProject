5693030a77f4c1b8daf0b4f9923b3c0356ecd63e
BalaMunde123 <balamunde123@gmail.com>
2022-11-11 12:32:31 +0530

    BOG1 :Create SprinBootProject

---
+++ src/main/java/com/example/FirstGitJiraProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGitJiraProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstGitJiraProjectApplication.class, args);
+	}
+
+}
